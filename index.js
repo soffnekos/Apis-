ff76fbdfe93fceace73516f32079279eff3504d7
DARK LEGEND <soffnekos@gmail.com>
2025-12-03 23:43:13 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('ᴍɪᴋᴜꜱ'ᴄʟᴜʙ', {
+say('ᴍɪᴋᴜꜱᴄʟᴜʙ', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
